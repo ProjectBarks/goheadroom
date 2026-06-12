7c835dc10c21287bd7dac7fdaa3613acc8e7d424
Brandon Barker <bbarker@uber.com>
2026-06-12 15:12:10 -0400

    fix: use custom build tag for HF tokenizer (avoids link errors without libtokenizers)

--- tokenizer/hf_stub.go
+++ tokenizer/hf_stub.go
@@ -1,4 +1,4 @@
-//go:build !cgo
+//go:build !hf_tokenizer
 
 package tokenizer
 
@@ -11,12 +11,12 @@ type HfTokenizer struct {
 
 // NewHfTokenizer returns an error when CGO is disabled.
 func NewHfTokenizer(name string, jsonBytes []byte) (*HfTokenizer, error) {
-	return nil, fmt.Errorf("HuggingFace tokenizer requires CGO (build with CGO_ENABLED=1)")
+	return nil, fmt.Errorf("HuggingFace tokenizer requires build tag hf_tokenizer (build with -tags hf_tokenizer)")
 }
 
 // NewHfTokenizerFromFile returns an error when CGO is disabled.
 func NewHfTokenizerFromFile(name, path string) (*HfTokenizer, error) {
-	return nil, fmt.Errorf("HuggingFace tokenizer requires CGO (build with CGO_ENABLED=1)")
+	return nil, fmt.Errorf("HuggingFace tokenizer requires build tag hf_tokenizer (build with -tags hf_tokenizer)")
 }
 
 func (t *HfTokenizer) CountText(text string) int { return 0 }
